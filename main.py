384e734ec57a2034048b0b3823c3de1690acfad9
niruhan <niruhanv@gmail.com>
2023-03-03 13:36:47 -0500

    Refactor dataloaders

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torchvision.models import resnet34
 from torch.utils.data import DataLoader
-
-# params
+from torchvision.transforms import transforms
 from OfficeDataset import OfficeDataset
 from model.basenet import Predictor
 from utils import get_classlist
@@ -24,15 +24,15 @@ data_dir = 'data/office'
 class_list = get_classlist(source_annotation_path)
 num_class = len(class_list)
 
-G = resnet34(pretrained=True)
-F = Predictor(num_class=num_class, input_vector_size=resnet_output_vector_size, norm_factor=temperature)
-nn.init.xavier_normal_(F.fc.weight)
+feature_extractor = resnet34(pretrained=True)
+predictor = Predictor(num_class=num_class, input_vector_size=resnet_output_vector_size, norm_factor=temperature)
+nn.init.xavier_normal_(predictor.fc.weight)
 # nn.init.zeros_(F.fc.bias)
 
-G = nn.DataParallel(G)
-F = nn.DataParallel(F)
-G = G.to(device)
-F = F.to(device)
+feature_extractor = nn.DataParallel(feature_extractor)
+predictor = nn.DataParallel(predictor)
+feature_extractor = feature_extractor.to(device)
+predictor = predictor.to(device)
 
 opt = {}
 opt['logs_file'] = logs_file
@@ -40,15 +40,23 @@ opt['checkpath'] = checkpath
 opt['class_list'] = class_list
 
 # load required data
-
-source_data_loader = DataLoader(OfficeDataset(source_annotation_path, data_dir))
-labled_target_data_loader = DataLoader(OfficeDataset(labled_target_annotation_path, data_dir))
-unlabled_target_data_loader = DataLoader(OfficeDataset(unlabled_target_annotation_path, data_dir))
-
-def train() :
-    G.train()
-    F.train()
+train_transform = transforms.Resize((256, 256))
+source_dataset = OfficeDataset(source_annotation_path, data_dir, transform=train_transform)
+source_data_loader = DataLoader(source_dataset, batch_size=batch_size, num_workers=0, shuffle=True, drop_last=True)
+labled_target_dataset = OfficeDataset(labled_target_annotation_path, data_dir, transform=train_transform)
+labled_target_data_loader = DataLoader(labled_target_dataset, batch_size=batch_size, num_workers=0, shuffle=True,
+                                       drop_last=True)
+unlabled_target_dataset = OfficeDataset(unlabled_target_annotation_path, data_dir, transform=train_transform)
+unlabled_target_data_loader = DataLoader(unlabled_target_dataset, batch_size=batch_size, num_workers=0, shuffle=True,
+                                         drop_last=True)
 
 
 
 print("hi")
+
+def train() :
+    feature_extractor.train()
+    predictor.train()
+
+    optimizer_feature_extractor = optim.SGD(feature_extractor.parameters(), lr=learning_rate)
+
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('macosx')
+
+
 def get_classlist(annotation_path):
     with open(annotation_path) as f:
         class_list = []
@@ -6,3 +11,19 @@ def get_classlist(annotation_path):
             if class_name not in class_list:
                 class_list.append(str(class_name))
         return class_list
+
+
+def display_images_from_dataloader(data_loader):
+    examples = enumerate(data_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    fig = plt.figure()
+    for i in range(4):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Ground Truth: {}".format(example_targets[i]))
+        plt.xticks([])
+        plt.yticks([])
+    fig
+    plt.show()
