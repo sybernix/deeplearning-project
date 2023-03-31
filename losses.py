Fix unlabeled loss calculation functions

losses.py
@@ -10,8 +10,8 @@ def get_losses_unlabeled(args, feature_extractor, predictor, unlabeled_data_imag
     features_transformed1 = feature_extractor(unlabeled_data_images_transformed1)
     features_transformed2 = feature_extractor(unlabeled_data_images_transformed2)
 
-    predictions = predictor(features, reversed=True, eta=1.0)
-    predictions_transformed1 = predictor(features_transformed1, reversed=True, eta=1.0)
+    predictions = predictor(features, reversed=True, lambd=1.0)
+    predictions_transformed1 = predictor(features_transformed1, reversed=True, lambd=1.0)
 
     probabilities = F.softmax(predictions, dim=1)
     probabilities_transformed1 = F.softmax(predictions_transformed1, dim=1)
@@ -30,7 +30,7 @@ def get_losses_unlabeled(args, feature_extractor, predictor, unlabeled_data_imag
     probabilities_transformed2 = F.softmax(predictions_transformed2, dim=1)
 
     max_probabilities, pseudo_labels = torch.max(probabilities.detach_(), dim=1)
-    mask = max_probabilities.get(args.threshold).float()
+    mask = max_probabilities.ge(args.threshold).float()
 
     pseudo_labels_loss = (F.cross_entropy(predictions_transformed2, pseudo_labels, reduction='none') * mask).mean()
 

model/basenet.py
@@ -7,15 +7,18 @@ class GradReverse(Function):
     def __int__(self, lambd):
         self.lambd = lambd
 
-    def forward(self, x, **kwargs):
+    @staticmethod
+    def forward(self, x):
         return x.view_as(x)
 
+    @staticmethod
     def backward(self, output_grad):
         input_grad = output_grad * (-self.lambd)
+        return input_grad
 
 
 def grad_reverse(x, lambd=1.0):
-    return GradReverse(lambd)(x)
+    return GradReverse(lambd).apply(x)
 
 
 class Predictor(nn.Module):
@@ -25,8 +28,8 @@ class Predictor(nn.Module):
         self.num_class = num_class
         self.norm_factor = norm_factor
 
-    def forward(self, x, reverse=False, lambd=0.1):
-        if reverse:
+    def forward(self, x, reversed=False, lambd=0.1):
+        if reversed:
             x = grad_reverse(x, lambd)
         x = F.normalize(x)
         x = self.fc(x)